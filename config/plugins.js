c27fdee9740038675146c96e0bb8fe3af3ca1d95
Manuel Ghizzoni <ghizzo.manuel@gmail.com>
2025-05-07 01:38:33 +0200

    fix(documentation): enable docs for i18n plugin

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,10 @@
-module.exports = () => ({});
+module.exports = () => ({
+  documentation: {
+    enabled: true,
+    config: {
+      "x-strapi-config": {
+        plugins: ["i18n"],
+      },
+    },
+  },
+});
